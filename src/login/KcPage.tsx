75fb37cb509b50ba4c981ecc47af53de1f698917
Dracowyn <dracowyn@outlook.com>
2026-01-22 19:59:54 +0800

    feat: add login reset password with phone functionality

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -4,6 +4,7 @@ import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
 import Template from "keycloakify/login/Template";
+import LoginResetPasswordWithPhone from "./pages/LoginResetPasswordWithPhone.tsx";
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
@@ -51,6 +52,14 @@ export default function KcPage(props: { kcContext: KcContext }) {
                                 doUseDefaultCss={true}
                                 UserProfileFormFields={UserProfileFormFields}
                                 doMakeUserConfirmPassword={doMakeUserConfirmPassword}
+                            />
+                        );
+                    case "login-reset-password-with-phone.ftl":
+                        return (
+                            <LoginResetPasswordWithPhone
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={true}
                             />
                         );
                 }
